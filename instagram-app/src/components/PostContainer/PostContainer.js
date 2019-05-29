269faf83158f58ef59ecc2f1135e02e2fbca4ff6
JacobWellner83 <jcwellner83@gmail.com>
2019-05-28 21:21:39 -0500

    framed structure for PostContainer

---
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import CommentSection from '../CommentSection/CommentSection';
+
+const PostContainer = () => {
+    return (
+        <div>
+            PostContainer
+            <CommentSection />
+        </div>
+    );
+};
+
+export default PostContainer;
